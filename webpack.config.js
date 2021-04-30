cc81a679185b4785bdc136b38a1853b6e8859555
Greta Schock <gschockcodes@gmail.com>
2021-04-30 12:58:21 -0700

    Successfully initialized portfolio_site :)

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 module.exports = {
-  entry: path.resolve(__dirname, 'client', 'src', 'index.jsx'),
+  entry: path.resolve(__dirname, 'client', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
