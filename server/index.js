db2022a0e0d89af0876bffcdf9260e490bda3a76
Greta Schock <gschockcodes@gmail.com>
2021-04-30 12:49:32 -0700

    Added server index file

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const axios = require('axios');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(express.static(PUBLIC_DIR));
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
