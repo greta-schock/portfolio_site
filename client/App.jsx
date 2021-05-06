6843ebcde2bed9a1a79d823ab27204d80f5b4335
Greta Schock <gschockcodes@gmail.com>
2021-05-05 19:11:30 -0700

    Added Nav bar

--- client/App.jsx
+++ client/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar';
 
 const App = () => {
     return (
-        <div>React is working!</div>
+        <>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">Greta Schock</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#about_me">About Me</Nav.Link>
+                    <Nav.Link href="#projects">Projects</Nav.Link>
+                    <Nav.Link href="#contact_me">Contact Me</Nav.Link>
+                </Nav>
+            </Navbar>
+        </>
     )
 }
 
