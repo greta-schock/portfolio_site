3f22d6a597ed192ab1c92bcdf24a70caa0191769
Greta Schock <gschockcodes@gmail.com>
2021-05-05 19:22:05 -0700

    Added new items to bundle

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+//Bootstrap Imports
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar';
+//Component Imports
+import Projects from './components/Projects.jsx';
+import ContactMe from './components/ContactMe.jsx';
 
 const App = () => {
     return (
@@ -8,11 +12,13 @@ const App = () => {
             <Navbar bg="dark" variant="dark">
                 <Navbar.Brand href="#home">Greta Schock</Navbar.Brand>
                 <Nav className="mr-auto">
-                    <Nav.Link href="#about_me">About Me</Nav.Link>
+                    <Nav.Link href="#tech_skills">Tech Skills</Nav.Link>
                     <Nav.Link href="#projects">Projects</Nav.Link>
                     <Nav.Link href="#contact_me">Contact Me</Nav.Link>
                 </Nav>
             </Navbar>
+            <Projects />
+            <ContactMe />
         </>
     )
 }
