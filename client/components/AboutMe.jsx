9ba6f4440c8eb7da1027e170b57302d7754ca42f
Greta Schock <gschockcodes@gmail.com>
2021-05-05 19:40:09 -0700

    Added intital AboutMe styling for background and text layering

---
+++ client/components/AboutMe.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AboutMe = () => {
+    return (
+        <>
+            <div id="about_me">
+                <div class="hero-text">
+                    <h1 style={{fontSize: "50px"}}>Hey there, I'm Greta Schock</h1>
+                    <h3>And I'm a Software Engineer</h3>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default AboutMe;
