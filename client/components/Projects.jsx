df436e1708d82fa78e23560ec53c8120d19158f6
Greta Schock <gschockcodes@gmail.com>
2021-05-13 18:44:02 -0700

    Added SmoveCarousel feature

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -3,6 +3,8 @@ import Card from 'react-bootstrap/Card';
 import CardDeck from 'react-bootstrap/CardDeck';
 import CardGroup from 'react-bootstrap/CardGroup';
 import Carousel from 'react-bootstrap/Carousel';
+//Carousels
+import SmoveCarousel from './projects_components/SmoveCarousel';
 
 const Projects = () => (
     <>
@@ -10,43 +12,7 @@ const Projects = () => (
         <CardDeck>
             <Card>
                 {/* <Card.Img variant="top" src="./photos/background.jpg" /> */}
-                <Carousel>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src="./photos/background.jpg"
-                            alt="First slide"
-                        />
-                        <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src="./photos/background.jpg"
-                            alt="Second slide"
-                        />
-
-                        <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src="./photos/background.jpg"
-                            alt="Third slide"
-                        />
-
-                        <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
+                <SmoveCarousel />
                 <Card.Body>
                     <Card.Title>Smove</Card.Title>
                     <Card.Text>
---
+++ client/components/projects_components/SmoveCarousel.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SmoveCarousel = () => {
+    return (
+        <div>SmoveCarousel is working!</div>
+    )
+}
+
+export default SmoveCarousel;
