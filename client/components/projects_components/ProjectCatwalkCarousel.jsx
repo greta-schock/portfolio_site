c7142da683473d43337e0ebf11ab7c87cf1f28f3
Greta Schock <gschockcodes@gmail.com>
2021-05-14 15:02:19 -0700

    Added Project Catwalk photos to Project Catwalk carousel

--- client/components/projects_components/ProjectCatwalkCarousel.jsx
+++ client/components/projects_components/ProjectCatwalkCarousel.jsx
@@ -6,37 +6,23 @@ const ProjectCatwalkCarousel = () => (
         <Carousel.Item>
             <img
                 className="d-block w-100"
-                src="./photos/background.jpg"
+                src="./photos/project_catwalk_screenshots/project_catwalk_product_overview.png"
                 alt="First slide"
             />
-            <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
             <img
                 className="d-block w-100"
-                src="./photos/background.jpg"
+                src="./photos/project_catwalk_screenshots/project_catwalk_QandA.png"
                 alt="Second slide"
             />
-
-            <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
             <img
                 className="d-block w-100"
-                src="./photos/background.jpg"
+                src="./photos/project_catwalk_screenshots/project_catwalk_RandR.png"
                 alt="Third slide"
             />
-
-            <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </Carousel.Caption>
         </Carousel.Item>
     </Carousel>
 );
