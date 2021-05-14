84c8b3a8cada8f7766626ee6cdf9c0c56c40d1b3
Greta Schock <gschockcodes@gmail.com>
2021-05-13 18:37:20 -0700

    Added in carousel as a card feature

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -9,20 +9,57 @@ const Projects = () => (
         <h1>Projects</h1>
         <CardDeck>
             <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
+                {/* <Card.Img variant="top" src="./photos/background.jpg" /> */}
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="./photos/background.jpg"
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="./photos/background.jpg"
+                            alt="Second slide"
+                        />
+
+                        <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="./photos/background.jpg"
+                            alt="Third slide"
+                        />
+
+                        <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
                 <Card.Body>
-                    <Card.Title>Card title</Card.Title>
+                    <Card.Title>Smove</Card.Title>
                     <Card.Text>
                         This is a wider card with supporting text below as a natural lead-in to
                         additional content. This content is a little bit longer.
-      </Card.Text>
+                    </Card.Text>
                 </Card.Body>
                 <Card.Footer>
                     <small className="text-muted">Last updated 3 mins ago</small>
                 </Card.Footer>
             </Card>
             <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Img variant="top" src="./photos/background.jpg" />
                 <Card.Body>
                     <Card.Title>Card title</Card.Title>
                     <Card.Text>
@@ -35,14 +72,14 @@ const Projects = () => (
                 </Card.Footer>
             </Card>
             <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Img variant="top" src="./photos/background.jpg" />
                 <Card.Body>
                     <Card.Title>Card title</Card.Title>
                     <Card.Text>
                         This is a wider card with supporting text below as a natural lead-in to
                         additional content. This card has even longer content than the first to
                         show that equal height action.
-      </Card.Text>
+                    </Card.Text>
                 </Card.Body>
                 <Card.Footer>
                     <small className="text-muted">Last updated 3 mins ago</small>
