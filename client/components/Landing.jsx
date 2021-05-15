60c48b7b353e85b4126a51206af72b5f3bbdf728
Greta Schock <gschockcodes@gmail.com>
2021-05-15 11:09:28 -0700

    Changed Landing to have a card layout instead of a actual photo

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+
 
 const Landing = () => (
-    <div id="about_me"></div>
+    // <div id="about_me"></div>
+    <Card className="bg-dark text-white">
+        <Card.Img src="./photos/gc2.jpeg" alt="Card image" />
+        <Card.ImgOverlay>
+            <h1>Greta Schock</h1>
+            <Card.Title>Card title</Card.Title>
+        
+        </Card.ImgOverlay>
+    </Card>
 
 )
 
