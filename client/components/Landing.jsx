de640c09424172df5ac9380f659e0bc59285dee1
Greta Schock <gschockcodes@gmail.com>
2021-05-15 16:15:46 -0700

    CSS changes to Landing

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -3,15 +3,16 @@ import Card from 'react-bootstrap/Card';
 
 
 const Landing = () => (
-    // <div id="about_me"></div>
     <Card className="bg-dark text-white">
-        <Card.Img src="./photos/gc2.jpeg" alt="Card image" />
-        <Card.ImgOverlay >
+        <Card.Img src="./photos/gc2.jpeg" alt="Card image" width="100%"/>
+        <Card.ImgOverlay id="about_me_text">
             <h1>Greta Schock</h1>
             {/* <Card.Title>Card title</Card.Title> */}
             <div>Software Engineer</div>
+            <div>Seattle, WA</div>
         </Card.ImgOverlay>
     </Card>
+
 
 )
 
