f0fb4518a13207a3b11dfbedbaa62856fc3cb9bd
Greta Schock <gschockcodes@gmail.com>
2021-05-17 14:46:58 -0700

    Added resume link

--- client/components/AboutMe.jsx
+++ client/components/AboutMe.jsx
@@ -18,6 +18,7 @@ const AboutMe = () => {
                         <p>
                         My name is Greta Schock and I am a Full-Stack Software Engineer. My interest in SWE stems from the human element of web development. After receiving my both Bachelor of Arts in International Affairs - Latin America concentration and Anthropolgy, but I have since shifted to software engineering to create accessible tools that will have a positive impact on the global community. Please feel free to reach out if you have any questions or would like to connect!
                         </p>
+                        <a href ="https://drive.google.com/file/d/1DowFaJe9AcpwBkowa8ifB_31yKTl7Sb8/view?usp=sharing" target="_blank" attributes-list> Resume via Google Docs </a>  
                     </Col>
                 </Row>
             </Container>
