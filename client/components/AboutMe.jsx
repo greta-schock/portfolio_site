ccfe12ed69747510c9ad92ec305d33a5923f1f62
Greta Schock <gschockcodes@gmail.com>
2021-05-17 13:04:50 -0700

    Added LinkedIn profile info to abotut me

--- client/components/AboutMe.jsx
+++ client/components/AboutMe.jsx
@@ -17,8 +17,8 @@ const AboutMe = () => {
                     <Col sm={7}>
                         <h1>Hello, world!</h1>
                         <p>
-                            Hey there! I'm Greta and I adore learning languages, both linguistic and code-based. "It's not that you don't know something, you don't know something yet."
-                            </p>
+                        Whale, hello world! 🐳 My name is Greta and I am a full-stack software engineer. I'm currently working on passion projects while also considering other employment opportunities. My interest stems from the human element of web development. My professional background thus far has been in guiding and tourism, but I have since shifted to software engineering to create accessible tools that will have a positive impact on the global community. Please feel free to reach out if you have any questions or would like to connect!
+                        </p>
 
                     </Col>
                 </Row>
