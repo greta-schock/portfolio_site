37b5d123daa8d1e6df66e7b7a7a2d08d8319e856
Greta Schock <gschockcodes@gmail.com>
2021-05-17 14:32:16 -0700

    Got rid of the column sizes to prevent overlap

--- client/components/AboutMe.jsx
+++ client/components/AboutMe.jsx
@@ -8,11 +8,12 @@ const AboutMe = () => {
     return (
         <>
             <Container id="about_me">
+                <h1>About Me</h1>
                 <Row>
-                    <Col sm={5}>
+                    <Col >
                         <Image id="profile_pic" src="./photos/profile_pic.jpeg" />
                     </Col>
-                    <Col sm={7}>
+                    <Col >
                         <h1>Hello, world!</h1>
                         <p>
                         Whale, hello world! 🐳 My name is Greta and I am a full-stack software engineer. I'm currently working on passion projects while also considering other employment opportunities. My interest stems from the human element of web development. My professional background thus far has been in guiding and tourism, but I have since shifted to software engineering to create accessible tools that will have a positive impact on the global community. Please feel free to reach out if you have any questions or would like to connect!
