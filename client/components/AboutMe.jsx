3d26a0c87f36af4d4a802b93b08523597cc04687
Greta Schock <gschockcodes@gmail.com>
2021-05-20 15:55:04 -0700

    Added first round of color changes

--- client/components/AboutMe.jsx
+++ client/components/AboutMe.jsx
@@ -7,9 +7,9 @@ import Row from 'react-bootstrap/Row';
 
 const AboutMe = () => {
     return (
-        <>
-                <h1>About Me</h1>
-            <Container id="about_me">
+        <div id="about_me">
+            <h1>About Me</h1>
+            <Container>
                 <Row>
                     <Col >
                         <Image id="profile_pic" src="./photos/profile_pic.jpeg" />
@@ -17,20 +17,20 @@ const AboutMe = () => {
                     <Col >
                         <h1>Whale, hello world! 🐳 </h1>
                         <p>
-                        My name is Greta Schock and I am a Full-Stack Software Engineer. My interest in SWE stems from the human element of web development. After receiving a Bachelor of Arts in both International Affairs - Latin America and Anthropolgy, I have shifted to software engineering to create accessible tools that will have a positive impact on the global community.
+                            My name is Greta Schock and I am a Full-Stack Software Engineer. My interest in SWE stems from the human element of web development. After receiving a Bachelor of Arts in both International Affairs - Latin America and Anthropolgy, I have shifted to software engineering to create accessible tools that will have a positive impact on the global community.
                         </p>
                         <p>
-                        Please feel free to reach out if you have any questions or would like to connect!
+                            Please feel free to reach out if you have any questions or would like to connect!
                         </p>
                         <Button href="https://drive.google.com/file/d/1DowFaJe9AcpwBkowa8ifB_31yKTl7Sb8/view?usp=sharing" target="_blank"><i className="fas fa-file"></i> Resume</Button>{' '}
                         <Button href="https://www.linkedin.com/in/greta-schock/" target="_blank"><i className="fab fa-linkedin-in"></i> LinkedIn</Button>{' '}
                         <Button href="https://github.com/grsc0529" target="_blank"><i className="fab fa-github"></i> GitHub</Button>{' '}
                         <Button href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=greta.schock@gmail.com" target="_blank"><i className="fas fa-envelope"></i> Gmail</Button>
                     </Col>
-                
+
                 </Row>
             </Container>
-        </>
+        </div>
     )
 }
 
