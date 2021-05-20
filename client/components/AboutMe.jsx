749d00b14d949899ef78d6edaa2f0f0a0a1853a7
Greta Schock <gschockcodes@gmail.com>
2021-05-20 15:21:49 -0700

    Editorial changes in writing

--- client/components/AboutMe.jsx
+++ client/components/AboutMe.jsx
@@ -17,7 +17,10 @@ const AboutMe = () => {
                     <Col >
                         <h1>Whale, hello world! 🐳 </h1>
                         <p>
-                        My name is Greta Schock and I am a Full-Stack Software Engineer. My interest in SWE stems from the human element of web development. After receiving a Bachelor of Arts in both International Affairs - Latin America and Anthropolgy, but I have shifted to software engineering to create accessible tools that will have a positive impact on the global community. Please feel free to reach out if you have any questions or would like to connect!
+                        My name is Greta Schock and I am a Full-Stack Software Engineer. My interest in SWE stems from the human element of web development. After receiving a Bachelor of Arts in both International Affairs - Latin America and Anthropolgy, I have shifted to software engineering to create accessible tools that will have a positive impact on the global community.
+                        </p>
+                        <p>
+                        Please feel free to reach out if you have any questions or would like to connect!
                         </p>
                         <Button href="https://drive.google.com/file/d/1DowFaJe9AcpwBkowa8ifB_31yKTl7Sb8/view?usp=sharing" target="_blank"><i className="fas fa-file"></i> Resume</Button>{' '}
                         <Button href="https://www.linkedin.com/in/greta-schock/" target="_blank"><i className="fab fa-linkedin-in"></i> LinkedIn</Button>{' '}
--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -8,7 +8,7 @@ const Landing = () => (
         <Card.Img id="background" src="./photos/gc2.jpeg" alt="Card image"/>
         <Card.ImgOverlay id="about_me_text">
             <h1>Greta Schock</h1>
-            <div>Software Engineer</div>
+            <div>Fullstack Software Engineer</div>
             <div>Seattle, WA</div>
         </Card.ImgOverlay>
     </Card>
