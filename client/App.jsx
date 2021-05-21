e577afb311a60a2f4770444934e15a2534f28508
Greta Schock <gschockcodes@gmail.com>
2021-05-21 10:43:54 -0700

    Added contact me button to nav bar

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 //Bootstrap Imports
-import Nav from 'react-bootstrap/Nav'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 //Component Imports
 import Landing from './components/Landing.jsx';
@@ -18,8 +20,13 @@ const App = () => {
                     <Nav.Link href="#about_me">About Me</Nav.Link>
                     <Nav.Link href="#tech_skills">Tech Skills</Nav.Link>
                     <Nav.Link href="#projects">Projects</Nav.Link>
-                    <Nav.Link href="#contact_me">Contact Me</Nav.Link>
+                    {/* <Nav.Link href="#contact_me">Contact Me</Nav.Link> */}
                 </Nav>
+                <Form inline>
+                    <Button href="https://www.linkedin.com/in/greta-schock/" target="_blank" size="sm" variant="light"><i className="fab fa-linkedin-in"></i></Button>
+                    <Button href="https://github.com/grsc0529" target="_blank" size="sm" variant="light"><i className="fab fa-github"></i></Button>
+                    <Button href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=greta.schock@gmail.com" target="_blank" size="sm" variant="light"><i className="fas fa-envelope"></i></Button>
+                </Form>
             </Navbar>
             <Landing />
             <div id="content">
