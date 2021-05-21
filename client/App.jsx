9a6d9b014064a06d75990582656eb5ec80756a2e
Greta Schock <gschockcodes@gmail.com>
2021-05-21 10:33:12 -0700

    Removed ContactMe (for now) and made buttons larger

--- client/App.jsx
+++ client/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
                 <br></br>
                 <Projects />
                 <br></br>
-                <ContactMe />
+                {/* <ContactMe /> */}
             </div>
         </>
     )
