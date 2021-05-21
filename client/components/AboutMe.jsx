96211dc151cab48dccf0a83bc330f36cd4d25f5c
Greta Schock <gschockcodes@gmail.com>
2021-05-21 08:27:52 -0700

    Font size and space formatting

--- client/components/AboutMe.jsx
+++ client/components/AboutMe.jsx
@@ -17,12 +17,16 @@ const AboutMe = () => {
                     </Col>
                     <Col >
                         <h1>Whale, hello world! 🐳 </h1>
-                        <p>
+                        <br></br>
+                        <br></br>
+                        <div>
                             My name is Greta Schock and I am a Full-Stack Software Engineer. My interest in SWE stems from the human element of web development. After receiving a Bachelor of Arts in both International Affairs - Latin America and Anthropolgy, I have shifted to software engineering to create accessible tools that will have a positive impact on the global community.
-                        </p>
-                        <p>
+                        </div>
+                        <br></br>
+                        <div>
                             Please feel free to reach out if you have any questions or would like to connect!
-                        </p>
+                        </div>
+                        <br></br>
                         <Button href="https://drive.google.com/file/d/1DowFaJe9AcpwBkowa8ifB_31yKTl7Sb8/view?usp=sharing" target="_blank"><i className="fas fa-file"></i> Resume</Button>{' '}
                         <Button href="https://www.linkedin.com/in/greta-schock/" target="_blank"><i className="fab fa-linkedin-in"></i> LinkedIn</Button>{' '}
                         <Button href="https://github.com/grsc0529" target="_blank"><i className="fab fa-github"></i> GitHub</Button>{' '}
