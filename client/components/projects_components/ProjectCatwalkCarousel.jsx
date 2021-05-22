0d64a29b84515b1d3bfe413b5d680dca28bf4e07
Greta Schock <gschockcodes@gmail.com>
2021-05-22 11:35:20 -0700

    Made the alt tags for Project Catwalk more descriptive  Your branch is ahead of 'origin/master' by 1 commit.

--- client/components/projects_components/ProjectCatwalkCarousel.jsx
+++ client/components/projects_components/ProjectCatwalkCarousel.jsx
@@ -7,21 +7,21 @@ const ProjectCatwalkCarousel = () => (
             <img
                 className="d-block w-100"
                 src="./photos/project_catwalk_screenshots/project_catwalk_product_overview.png"
-                alt="First slide"
+                alt="First Project Catwalk slide - Product Overview"
             />
         </Carousel.Item>
         <Carousel.Item>
             <img
                 className="d-block w-100"
                 src="./photos/project_catwalk_screenshots/project_catwalk_QandA.png"
-                alt="Second slide"
+                alt="Second Project Catwalk slide - Questions and Answers"
             />
         </Carousel.Item>
         <Carousel.Item>
             <img
                 className="d-block w-100"
                 src="./photos/project_catwalk_screenshots/project_catwalk_RandR.png"
-                alt="Third slide"
+                alt="Third Project Catwalk slide - Ratins and Reviews"
             />
         </Carousel.Item>
     </Carousel>
