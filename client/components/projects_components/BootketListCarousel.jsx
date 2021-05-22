b40bdceeb2aebd0a6c0b05d82fcb3e5d0566c047
Greta Schock <gschockcodes@gmail.com>
2021-05-22 11:40:09 -0700

    Added better descriptions for Bootket List carousel images and rename one photo

--- client/components/projects_components/BootketListCarousel.jsx
+++ client/components/projects_components/BootketListCarousel.jsx
@@ -7,28 +7,28 @@ const BootketListCarousel = () => (
                 <img
                     className="d-block w-100"
                     src="./photos/bootket_list_screenshots/bootket_list_explore.png"
-                    alt="First slide"
+                    alt="First Bootket List slide - Explore page"
                 />
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100"
                     src="./photos/bootket_list_screenshots/bootket_list_list.png"
-                    alt="Second slide"
+                    alt="Second Bootket List slide - My Bootket List page"
                 />
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src="./photos/bootket_list_screenshots/bootket_list_LNT.png"
-                    alt="Third slide"
+                    src="./photos/bootket_list_screenshots/bootket_list_seeMoreInfo_modal.png"
+                    alt="Third Bootket List slide - See More Information modal"
                 />
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100"
                     src="./photos/bootket_list_screenshots/bootket_list_lntmodal.png"
-                    alt="Third slide"
+                    alt="Fourth Bootket List slide - Leave No Trace modal"
                 />
             </Carousel.Item>
         </Carousel>
