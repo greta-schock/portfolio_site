1f8c085e9778a4a1b8661580b05dadbc53263f88
Greta Schock <gschockcodes@gmail.com>
2021-05-26 16:43:15 -0700

    Code clean up

--- server/index.js
+++ server/index.js
@@ -1,31 +1,14 @@
 const express = require('express');
-// const morgan = require('morgan');
-// const axios = require('axios');
 const path = require('path');
 const app = express();
-// const port = 3000;
-// const app = require('express')();
 const PORT = process.env.PORT || 8080;
 
 const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
 
-
 app.use(express.json());
-// app.use(morgan('dev'));
 app.use(express.static(PUBLIC_DIR));
 
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
 })
-
-// const app = require("express")();
-// const PORT = process.env.PORT || 3000;
-
-// app.get("", (req, res) => {
-//   res.send('Hello World');
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`App up at port ${PORT}`)
-// })
