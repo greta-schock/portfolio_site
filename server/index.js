0f64ea8f81e8d469e4db7e555c0d25c5858cd4e1
Greta Schock <gschockcodes@gmail.com>
2021-05-26 15:55:32 -0700

    Reformatted server

--- server/index.js
+++ server/index.js
@@ -1,29 +1,31 @@
 // const express = require('express');
 // const morgan = require('morgan');
 // const axios = require('axios');
-// const path = require('path');
+const path = require('path');
 // const app = express();
 // const port = 3000;
-
-// const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
-
-
-// app.use(express.json());
-// // app.use(morgan('dev'));
-// app.use(express.static(PUBLIC_DIR));
-
-
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`)
-// })
-
-const app = require("express")();
+const app = require('express')();
 const PORT = process.env.PORT || 3000;
 
-app.get("", (req, res) => {
-  res.send('Hello World');
-});
+const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
+
+
+app.use(express.json());
+// app.use(morgan('dev'));
+app.use(express.static(PUBLIC_DIR));
+
 
 app.listen(PORT, () => {
-  console.log(`App up at port ${PORT}`)
+  console.log(`Example app listening at http://localhost:${PORT}`)
 })
+
+// const app = require("express")();
+// const PORT = process.env.PORT || 3000;
+
+// app.get("", (req, res) => {
+//   res.send('Hello World');
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`App up at port ${PORT}`)
+// })
