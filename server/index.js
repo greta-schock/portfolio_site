155ab65e14dc8fc214f30c3a6878678a7bfb74a7
Greta Schock <gschockcodes@gmail.com>
2021-05-26 13:27:11 -0700

    Changed server to look like the one in freeCodeCamp

--- server/index.js
+++ server/index.js
@@ -1,18 +1,29 @@
-const express = require('express');
-const morgan = require('morgan');
-const axios = require('axios');
-const path = require('path');
-const app = express();
-const port = 3000;
+// const express = require('express');
+// const morgan = require('morgan');
+// const axios = require('axios');
+// const path = require('path');
+// const app = express();
+// const port = 3000;
 
-const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
-
-
-app.use(express.json());
-// app.use(morgan('dev'));
-app.use(express.static(PUBLIC_DIR));
+// const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
 
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+// app.use(express.json());
+// // app.use(morgan('dev'));
+// app.use(express.static(PUBLIC_DIR));
+
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
+
+const app = require("express")();
+const PORT = process.env.PORT || 3000;
+
+app.get("", (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+  console.log(`App up at port ${PORT}`)
 })
