36ff884b4c07ad035a059e4bdf7513403b852717
Greta Schock <63438948+greta-schock@users.noreply.github.com>
2021-06-30 09:07:25 -0700

    Update Landing.jsx

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -8,7 +8,7 @@ const Landing = () => (
             <Card.Img id="background" src="./photos/gc2.jpeg" alt="Card image" />
             <Card.ImgOverlay id="landing_title">
                 <h1 style={{fontSize: "5vw"}}>Greta Schock</h1>
-                <div style={{fontSize: "2vw"}}>Fullstack Software Engineer</div>
+                <div style={{fontSize: "2vw"}}>Full Stack Software Engineer</div>
                 <div style={{fontSize: "2vw"}}>Seattle, WA</div>
             </Card.ImgOverlay>
         </Card>
